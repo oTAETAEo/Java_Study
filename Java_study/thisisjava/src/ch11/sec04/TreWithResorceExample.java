819d4484f6b2e888957fbc354ea3a023e0e84026
CKIRUser <CKIRUser@813508>
2024-02-27 20:25:44 +0900

    예외 처리 ...

---
+++ Java_study/thisisjava/src/ch11/sec04/TreWithResorceExample.java
@@ -0,0 +1,62 @@
+package ch11.sec04;
+
+public class TreWithResorceExample {
+
+	public static void main(String[] args) throws Exception{
+		
+		MyResource res1 = null;
+		
+		try {
+			// 리소스 열기.
+			 res1 = new MyResource("res1");
+			// 리소스 읽기
+			System.out.println(Integer.parseInt(res1.read1()));
+			System.out.println(Integer.parseInt(res1.read2()));
+			
+		} catch (Exception e) {
+			e.getMessage();
+			
+		} finally {
+			// 리소스 닫기 (항상 실행)
+			// 무조껀 닫아야 한다.
+			res1.close();			
+		}
+		
+		
+		// AutoCloseable 를 상속 받았다면 close() 는 생략 가능.
+		try (MyResource res2 = new MyResource("res2")){
+			// 리소스 열기.
+			// 리소스 읽기
+			System.out.println(Integer.parseInt(res2.read1()));
+			System.out.println(Integer.parseInt(res2.read2()));
+			
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		
+		// 파일 여는 코드가 길다면 이렇게도 가능하다.
+		// 위와 같이 close()는 생략 가능하다.
+		MyResource res3 = new MyResource("res3");
+		try (res3){
+			// 리소스 열기.
+			// 리소스 읽기
+			System.out.println(Integer.parseInt(res3.read1()));
+			System.out.println(Integer.parseInt(res3.read2()));
+			
+		} catch (Exception e) {
+			e.getMessage();
+		}
+	
+		MyResource res4 = new MyResource("res4");
+		MyResource res5 = new MyResource("res5");
+		try (res4;res5){
+			System.out.println(Integer.parseInt(res4.read1()));
+			System.out.println(Integer.parseInt(res5.read1()));
+			
+		} catch (Exception e) {
+			e.getMessage();
+		}
+				
+	}
+
+}
